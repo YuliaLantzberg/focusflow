e58046fe98a56f4bb87f8324be1cc79fe3741297
Yulia Lantzberg <lanzyuka@gmail.com>
2025-11-18 17:46:47 +0200

    chore: scaffold Tasks module and CRUD resource

--- focusflow-backend/src/app.module.ts
+++ focusflow-backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [PrismaModule, ProjectsModule],
+  imports: [PrismaModule, ProjectsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
