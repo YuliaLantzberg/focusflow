505cd91859746f7f4eba2d17c2399234e226ffc1
Yulia Lantzberg <lanzyuka@gmail.com>
2025-11-21 19:41:51 +0200

    feat(auth): add JWT strategy and guard for route protection

--- focusflow-backend/src/auth/auth.module.ts
+++ focusflow-backend/src/auth/auth.module.ts
@@ -2,12 +2,16 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 // Entities modules
 import { UsersModule } from 'src/users/users.module';
 
 // Services
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+// Controllers
 import { AuthController } from './auth.controller';
 
 @Module({
@@ -18,8 +22,9 @@ import { AuthController } from './auth.controller';
       signOptions: { expiresIn: '1d' },
     }),
     ConfigModule,
+    PassportModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
