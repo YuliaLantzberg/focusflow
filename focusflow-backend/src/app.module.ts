bf6f490705bdd928cc7a3ce8e46a65d6144af797
Yulia Lantzberg <lanzyuka@gmail.com>
2025-11-21 16:05:57 +0200

    chore(users): setup UsersModule with Prisma and export UsersService

--- focusflow-backend/src/app.module.ts
+++ focusflow-backend/src/app.module.ts
@@ -5,9 +5,18 @@ import { ProjectsModule } from './projects/projects.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
 import { NotesModule } from './notes/notes.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ProjectsModule, TasksModule, NotesModule],
+  imports: [
+    PrismaModule,
+    ProjectsModule,
+    TasksModule,
+    NotesModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
