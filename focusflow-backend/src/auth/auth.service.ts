3bd2ead9387f3de5e41c22897d7763864e6bdaa6
Yulia Lantzberg <lanzyuka@gmail.com>
2025-11-21 17:26:16 +0200

    chore(auth): add bcrypt setup to AuthService

--- focusflow-backend/src/auth/auth.service.ts
+++ focusflow-backend/src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  private readonly saltRounds = 10;
+}
