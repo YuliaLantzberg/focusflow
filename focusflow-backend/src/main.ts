8ba4ed0aaeaf9d846db6884150a90578e86a4b6a
Yulia Lantzberg <lanzyuka@gmail.com>
2025-11-22 23:13:11 +0200

    auth(backend): enable CORS for frontend integration

--- focusflow-backend/src/main.ts
+++ focusflow-backend/src/main.ts
@@ -13,6 +13,12 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
