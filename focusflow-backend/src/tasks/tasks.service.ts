2a5dcf28db54f100fa59c3cecb836c674d956519
Yulia Lantzberg <lanzyuka@gmail.com>
2025-12-24 00:00:39 +0200

    feat(tasks): integrate renormalization into move logic

--- focusflow-backend/src/tasks/tasks.service.ts
+++ focusflow-backend/src/tasks/tasks.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { MoveTaskDto } from './dto/move-task.dto';
-import { TaskStatus } from '@prisma/client';
+import { Task, TaskStatus } from '@prisma/client';
 
 @Injectable()
 export class TasksService {
@@ -91,13 +91,104 @@ export class TasksService {
     });
   }
 
-  move(id: string, moveTaskDto: MoveTaskDto) {
+  async move(movingTaskId: string, moveTaskDto: MoveTaskDto) {
     const { status, order } = moveTaskDto;
+    const GAP = 100;
+    let gapBelow = 0;
+    let gapAbove = 0;
+    let collision = false;
+    let prev: Task | null = null;
+    let next: Task | null = null;
+    let didRenormalized = false;
+
+    const task = await this.prisma.task.findUnique({
+      where: { id: movingTaskId },
+    });
+    if (!task) return;
+    const statusChanged = status !== undefined && status !== task.status;
+
+    if (statusChanged && order === undefined)
+      throw new BadRequestException('When changing status, order is required');
+
+    const targetStatus: TaskStatus | undefined = status ?? task.status;
+
+    let finalOrder = order;
+    const requestedOrder = order;
+    const filter = {
+      projectId: task.projectId,
+      status: targetStatus,
+      id: { not: movingTaskId },
+    };
+
+    if (requestedOrder !== undefined) {
+      // Check if there are tasks within same project and with the same order
+      const collisionTask = await this.prisma.task.findFirst({
+        where: { ...filter, order: requestedOrder },
+      });
+      collision = !!collisionTask;
+
+      prev = await this.prisma.task.findFirst({
+        where: { ...filter, order: { lt: requestedOrder } },
+        orderBy: {
+          order: 'desc',
+        },
+      });
+
+      next = await this.prisma.task.findFirst({
+        where: { ...filter, order: { gt: requestedOrder } },
+        orderBy: {
+          order: 'asc',
+        },
+      });
+
+      if (prev) gapBelow = requestedOrder - prev.order;
+      if (next) gapAbove = next.order - requestedOrder;
+    }
+
+    if (
+      (requestedOrder !== undefined && requestedOrder <= 0) ||
+      collision === true ||
+      (prev && gapBelow < GAP) ||
+      (next && gapAbove < GAP)
+    ) {
+      didRenormalized = true;
+      await this.renormalizeColumn(task.projectId, targetStatus);
+    }
+
+    // find final new order of the moving task
+    if (requestedOrder !== undefined) {
+      if (didRenormalized) {
+        prev = await this.prisma.task.findFirst({
+          where: { ...filter, order: { lt: requestedOrder } },
+          orderBy: {
+            order: 'desc',
+          },
+        });
+
+        next = await this.prisma.task.findFirst({
+          where: { ...filter, order: { gt: requestedOrder } },
+          orderBy: {
+            order: 'asc',
+          },
+        });
+      }
+
+      if (prev && next) {
+        finalOrder = Math.floor((prev.order + next.order) / 2);
+      } else if (prev && !next) {
+        finalOrder = prev.order + 1000;
+      } else if (!prev && next) {
+        finalOrder = next.order - 1000;
+      } else {
+        finalOrder = 1000;
+      }
+    }
+
     return this.prisma.task.update({
-      where: { id },
+      where: { id: movingTaskId },
       data: {
-        ...(order !== undefined && { order }),
-        ...(status !== undefined && { status }),
+        ...(status !== undefined && { status: targetStatus }),
+        ...(order !== undefined && { order: finalOrder }),
       },
     });
   }
